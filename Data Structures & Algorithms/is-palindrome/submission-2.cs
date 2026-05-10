297b89c73dbd80bce38b67a13fcc5e04fbaeb84f
Nikodem Stecko <nikodem.stecko@gmail.com>
2026-05-10 21:03:38 +0200

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        int start = 0;
+        int end = s.Length -1;
+
+        while(end > start)
+        {
+            if(!Char.IsLetterOrDigit(s[start]))
+            {
+                start++;
+                continue;
+            }
+            if(!Char.IsLetterOrDigit(s[end]))
+            {
+                end--;
+                continue;
+            }
+
+            if(Char.ToLower(s[start]) != Char.ToLower(s[end]))
+            {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
